1102310686d24a570a32e9a4bb912dafb9b336c4
sugarpunch-maker <sugar.punch.310@gmail.com>
2026-03-27 17:19:44 +0800

    Add files via upload

--- run_app.py
+++ run_app.py
@@ -1,37 +1,29 @@
 import subprocess
-import sys
-import time
 import webbrowser
+import time
 import socket
-import os
 
 PORT = 8501
-
-def get_app_path():
-    if getattr(sys, 'frozen', False):
-        return os.path.join(sys._MEIPASS, "app.py")
-    return "app.py"
 
 def is_port_open(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(("127.0.0.1", port)) == 0
 
-if __name__ == "__main__":
+# Streamlit起動（ローカル環境）
+subprocess.Popen([
+    "streamlit",
+    "run",
+    "app.py",
+    "--server.port=8501"
+])
 
-    app_path = get_app_path()
+# 起動待ち
+for _ in range(15):
+    time.sleep(1)
+    if is_port_open(PORT):
+        break
 
-    # Streamlit起動
-    process = subprocess.Popen(
-        [sys.executable, "-m", "streamlit", "run", app_path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-
-    # 起動待機
-    for _ in range(15):
-        time.sleep(1)
-        if is_port_open(PORT):
-            webbrowser.open(f"http://localhost:{PORT}")
-            break
+# ブラウザ起動
+webbrowser.open(f"http://localhost:{PORT}")
 
 
